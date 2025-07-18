e966191dfc4e326adb77cd5198a3a902236ba212
Lucas Monteiro <159576256+odevlucass@users.noreply.github.com>
2025-07-18 00:44:06 +0000

    salvando projeto

---
+++ script.js
@@ -0,0 +1,8 @@
+// variavel com valor do input
+// variavel com o valor "mensagem"
+
+//função para verificar se o input está em branco ou não e deixar tudo minusculo
+
+//função principal para verificar se é palidromo ou não
+
+//função para fazer o restart
